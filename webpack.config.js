af2777cd34dca22a39a59eae1faea5f9c912839a
sho-watanabe <sho-desukedo@sky.plala.or.jp>
2020-07-18 16:49:18 +0900

    add babel and image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const loader = require('sass-loader');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname,'./dist'),
@@ -12,6 +15,20 @@ module.exports = {
   module:{
     rules: [
       {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options:  {
+              presets: [
+                ['@babel/preset-env',{'targets': '> 0.25%, not dead'}],
+              ],
+            },
+          },
+        ],
+      },
+      {
         test: /\.(css|sass|scss)/,
         use: [
           {
@@ -19,23 +36,35 @@ module.exports = {
           },
           {
             loader: 'css-loader',
+            options: {
+              sourceMap: false,
+            },
           },
           {
             loader: 'sass-loader',
           },
-        ]
+        ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
               name: 'images/[name].[ext]'
-            }
+            },
           },
-        ]
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.pug/,
@@ -47,11 +76,11 @@ module.exports = {
             loader: 'pug-html-loader',
             options: {
               pretty: true,
-            }
-          }
-        ]
-      }
-    ]
+            },
+          },
+        ],
+      },
+    ],
   },
 plugins: [
   new MiniCssExtractPlugin({
